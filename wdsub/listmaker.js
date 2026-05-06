e2304f30794aff404e14e430603e9b06cad920d1
yavpicciotti <student.dp@youngsvilleacademy.org>
2026-05-06 12:53:52 -0400

    Create listmaker.js

---
+++ wdsub/listmaker.js
@@ -0,0 +1,45 @@
+const CHECKBOX = "<button onclick='uncrossItem(this)'><svg viewBox='0 0 448 512' width='13' title='check-square'><path d='M400 480H48c-26.51 0-48-21.49-48-48V80c0-26.51 21.49-48 48-48h352c26.51 0 48 21.49 48 48v352c0 26.51-21.49 48-48 48zm-204.686-98.059l184-184c6.248-6.248 6.248-16.379 0-22.627l-22.627-22.627c-6.248-6.248-16.379-6.249-22.628 0L184 302.745l-70.059-70.059c-6.248-6.248-16.379-6.248-22.628 0l-22.627 22.627c-6.248 6.248-6.248 16.379 0 22.627l104 104c6.249 6.25 16.379 6.25 22.628.001z' /></svg></button>";
+
+const UNCHECKBOX = "<button onclick='crossItem(this)'><svg viewBox='0 0 448 512' width='13' title='square'><path d='M400 32H48C21.5 32 0 53.5 0 80v352c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V80c0-26.5-21.5-48-48-48z' /></svg></button>";
+
+let itemInput = document.getElementById("item-input");
+let list = document.getElementById("list");
+
+//itemInput.focus();
+
+document.addEventListener("keydown", (key) => {
+  console.log(key.code);
+  if (key.code == "Enter") addItem(key)
+});
+
+function addItem(event) {
+  if (itemInput.value != "") {
+    let _newItem = itemInput.value;
+    let _elem = document.createElement("li");
+    _elem.innerText = _newItem;
+    _elem.innerHTML =  UNCHECKBOX + _elem.innerHTML;
+    list.append(_elem);
+    itemInput.value = "";
+    itemInput.focus();
+ }
+}
+  
+  // the quotes creates an empty string
+  
+
+function clearList(event) {
+list.innerHTML = "";
+}
+
+function crossItem(elem) {
+  let parentLI = elem.parentElement;
+   parentLI.style.textDecoration = "solid Black 0.2em line-through";
+  parentLI.innerHTML = CHECKBOX + parentLI.innerText;
+  
+}
+
+function uncrossItem(elem) {
+  let parentLI = elem.parentElement;
+  parentLI.style.textDecoration = "none";
+  parentLI.innerHTML = UNCHECKBOX + parentLI.innerText;
+}
